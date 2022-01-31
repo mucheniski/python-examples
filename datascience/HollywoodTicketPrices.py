489ca82830632cd18a41c1fe3c047c74ff0b859e
Diego Mucheniski <mucheniski@gmail.com>
2022-01-31 19:31:41 -0300

    Finishing and saving graphic

--- datascience/HollywoodTicketPrices.py
+++ datascience/HollywoodTicketPrices.py
@@ -5,7 +5,18 @@ pricesdf = pd.read_csv('../datasets/AnnualTicketSales.csv')
 pricesdf = pricesdf.sort_values('AVERAGE TICKET PRICE', ascending=True)
 
 plt.title('Price Average Through the Years')
+plt.xlabel('Years')
+plt.ylabel('Prices')
+plt.plot(
+    pricesdf['YEAR'], # Eixo X
+    pricesdf['AVERAGE TICKET PRICE'], # Eixo Y
+    'r.--', # cor e tipo da linha, vermelha pontilhada e com traços entre os pontos
+    label='Price'
+)
 
-plt.plot(pricesdf['YEAR'], pricesdf['AVERAGE TICKET PRICE'])
+plt.legend()
+
+# Saving the graphic
+plt.savefig('../exported/ticketPrices.png')
 
 plt.show()
